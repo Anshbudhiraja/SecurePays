a64d084e0f46b867afa17c9064bc582ef3b8b90f
Anshbudhiraja <anshbudhiraja77@gmail.com>
2026-01-02 11:31:07 +0530

    Updated Auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const {loginUser,verifyUser} = require("../controller/userController")
+const {loginUser,verifyUser, checkUserDetails, updateUserDetails} = require("../controller/userController")
 
 router.post("/login",loginUser)
 router.post("/verify",verifyUser)
+router.get("/checkUserDetails",checkUserDetails)
+router.put("/updateUserDetails",updateUserDetails)
 
 module.exports = router
